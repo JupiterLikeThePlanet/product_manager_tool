f510f02192590094a8365d87e14cbe02f335fd7f
Jupiter <jupiterbaudot@gmail.com>
2018-01-21 23:23:58 -0800

    Create controllers, routes, seed data

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,6 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
-    render component: 'Products', props: {products: @products}
   end
 
   # GET /products/1
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
   # root 'welcome#index'
-  root 'products#index'
+  root 'products#new'
   resources :products #, only: [:index]
   resources :properties #, only: [:index]
   resources :productproperties #, except: [:index] do
